48b8c83ef5d2eabdeb5919bea064fdbcea9bea27
amory <pnv170691@gmail.com>
2022-05-23 09:59:39 +0700

    update

---
+++ src/CreateAccountForm.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+const CreateAccountForm =()=>{
+    return <Wrapper>
+        <p></p>
+        <input type={"text"}/>
+    </Wrapper>
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin: 0 auto;
+  flex-wrap: wrap;
+  max-width: 1400px;
+  justify-content: center;
+  align-items: center;
+`
+export default CreateAccountForm;
